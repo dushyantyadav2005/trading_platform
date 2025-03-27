b3d7a85ea09bba07d563002c51a8ce6ebd149602
e <dyadav15112005@gmail.com>
2025-03-27 23:55:01 +0530

    mongo-setup

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express =require("express");
+const mongoose=require("mongoose");
+
+const PORT=process.env.PORT||3002;
+const url=process.env.MONGO_URL;
+// console.log(url);
+
+
+const app=express();
+
+app.listen(PORT,()=>{
+    console.log("app started");
+    mongoose.connect(url);
+    console.log("DB Connected!");
+
+})
