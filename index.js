e9be7e420d9c13fe99d29c92307d556afc19633d
dushyantyadav2005 <dyadav15112005@gmail.com>
2025-06-22 01:32:28 +0530

    final adding

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 let corsOptions = {
-  origin: ["https://dashboard-trading-platform-1.onrender.com", "http://localhost:3000"],
+  origin: ["https://dashboard-trading-platform-1.onrender.com", "https://fronttrading-platform-1.onrender.com"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
